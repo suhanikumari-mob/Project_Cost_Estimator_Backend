79333ba47bd5603f254f6ee91e5ed55f0fcf3c42
suhanikumari-mobcoder <suhani.kumari@mobcoder.com>
2026-06-25 15:42:03 +0530

    Add lifespan management to FastAPI app
    Refactor FastAPI initialization to include lifespan management for database table setup.

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,18 +1,34 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from app.repositories.user_repository import ensure_tables
 from app.router.estimation import router as estimate_router
 
-app = FastAPI(
-    title="Cost Estimation API"
-)
+# app = FastAPI(
+#     title="Cost Estimation API"
+# )
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await ensure_tables()
+    yield
+
+app = FastAPI(lifespan=lifespan)
+
 from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000","https://project-cost-estimator-frontend.vercel.app",
+        "http://localhost:3000","https://project-cost-estimator-frontend.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 app.include_router(estimate_router)
+
+
+
+
+
